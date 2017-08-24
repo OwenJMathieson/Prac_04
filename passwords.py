6563edc14f00aad850a65996302f0a6b2a189ca0
OwenJMathieson <owenisthe1@yahoo.com.au>
2017-08-24 11:18:59 +1000

    Well, that was easy.

---
+++ passwords.py
@@ -0,0 +1,10 @@
+def main():
+
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45''BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    username =  input("Enter Username: ")
+    if username in usernames:
+        print("Access granted")
+    else:
+        print("Access denied")
+
+main()
